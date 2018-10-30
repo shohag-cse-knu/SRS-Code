b1cf0f940086047d616e86e268b2ba2f48cdea5b
Syfur Rahaman Shohag <shohag02011992@yahoo.com>
2018-10-31 03:57:30 +0600

    Update sql_stored_function_column_retrieve_by_id.sql
    Mysql stored function that retrieves customer full id according to the primary key or id of that table

--- sql_stored_function_column_retrieve_by_id.sql
+++ sql_stored_function_column_retrieve_by_id.sql
@@ -2,13 +2,18 @@ DELIMITER $$
 
 USE `test`$$
 
-DROP FUNCTION IF EXISTS `get_quantity`$$
+DROP FUNCTION IF EXISTS `customer_full_id`$$
 
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_quantity`(PID INT) RETURNS VARCHAR(50) CHARSET latin1
+CREATE DEFINER=`root`@`localhost` 
+       FUNCTION `customer_full_id`(PID INT) RETURNS VARCHAR(50) CHARSET latin1
 BEGIN
-	DECLARE nam VARCHAR(50);
-	SELECT cust_full_id INTO nam FROM test.app_customer WHERE id = PID;
-	RETURN nam;
+	DECLARE customer_id VARCHAR(50);
+	SELECT cust_full_id INTO customer_id FROM test.app_customer WHERE id = PID;
+	RETURN customer_id;
 END$$
 
 DELIMITER ;
+
+-- RUn this query to retrieve data from function
+-- id_of_that_table = 1
+-- SELECT customer_full_id(id_of_that_table) customer_id; 
